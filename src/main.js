f560768f3d70f451b3a1cc7e35fb2c8d7a7430cf
caesiumy <mn0658@naver.com>
2020-09-18 21:31:44 +0900

    feat: Add router on my own

--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+const Login = {
+  template: "<div>Login Page</div>"
+};
+
+const routes = {
+  "/": App,
+  "/login": Login
+};
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  el: "#app",
+  computed: {
+    VueComponent() {
+      console.log(
+        "route",
+        routes[window.location.pathname] || {
+          template: "<div>Page Not Found</div>"
+        }
+      );
+      return (
+        routes[window.location.pathname] || {
+          template: "<div>Page Not Found</div>"
+        }
+      );
+    }
+  },
+  render(h) {
+    h(this.VueComponent);
+  }
+});
