fb4fd30073e62b818d1e442b5f97ce4a9f603f0b
caesiumy <mn0658@naver.com>
2020-09-21 21:18:07 +0900

    refactor: Separate to components

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,18 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "../App.vue";
 
+import Home from "../components/Home.vue";
+import Login from "../components/Login.vue";
+import NotFound from "../components/NotFound.vue";
+
 Vue.use(VueRouter);
-
-const Login = {
-  template: "<div>Login Page</div>"
-};
-
-const NotFound = {
-  template: "<div>Page Not Found</div>"
-};
 
 const router = new VueRouter({
   mode: "history",
   routes: [
     {
       path: "/",
-      component: App
+      component: Home
     },
     {
       path: "/login",
