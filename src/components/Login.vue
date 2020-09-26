76f77b166cb5c7f1eb1f946344d300da5af11b32
caesiumy <mn0658@naver.com>
2020-09-26 17:55:07 +0900

    feat: Update Login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,73 @@
 <template>
-  <div>
-    Login
+  <div class="login">
+    <h2>Log in to Trello</h2>
+    <form @submit.prevent="onSubmit">
+      <div>
+        <label for="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          class="form-control"
+          v-model="email"
+          autofocus
+          placeholder="e.g., test@test.com"
+        />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          class="form-control"
+          v-model="password"
+          placeholder="123123"
+        />
+      </div>
+
+      <button
+        class="btn"
+        :class="{ 'btn-success': !invalidForm }"
+        :disabled="invalidForm"
+      >
+        Log In
+      </button>
+    </form>
+
+    <p class="error" v-if="error">{{ error }}</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: ""
+    };
+  },
+
+  computed: {
+    invalidForm() {
+      return !this.email || !this.password;
+    }
+  },
+
+  methods: {
+    onSubmit() {
+      console.log(this.email, this.password);
+    }
+  }
+};
 </script>
 
-<style></style>
+<style>
+.login {
+  width: 400px;
+  margin: 0 auto;
+}
+
+.error {
+  color: #f00;
+}
+</style>
